924f54aedb0030db2f7bbcfd3450ca88ece94729
harshita <harshita.singh@mcbirdtechnologies.com>
2026-06-15 23:33:18 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app= express();
 
-app.use("/",(req, res)=>{res.send('Middleware 1');})
+app.use("/test",(req, res)=>{res.send('test 2');})
+app.use("/hello",(req, res)=>{res.send('Middleware 1');})
+
 
 app.listen(3000,()=>{console.log('Server is running on port 3000')});
